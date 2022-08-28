cd527f325ff7f1087470ddf9b05f4966832e8930
Ofir <ofir12121@gmail.com>
2022-08-28 16:59:27 +0300

    Change initial checkBoxes

--- app.js
+++ app.js
@@ -20,16 +20,19 @@ const itemsSchema = {
 const Item = new mongoose.model("Item", itemsSchema);
 
 const item1 = new Item({
-  name: "Get food"
+  name: "Welcome to your todolist"
 });
 
 const item2 = new Item({
-  name: "Cook food"
+  name: "Hit the + button to add a new item"
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item"
 });
 
 
 
-const defaultItems = [item1, item2];
+const defaultItems = [item1, item2,item3];
 
 const listSchema = {
   name: String,
